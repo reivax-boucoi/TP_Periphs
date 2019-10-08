f06be9a192652e200e679ef86c0275400ff87014
reivax-boucoi <reivax.boucoi@laposte.net>
2019-10-08 16:09:44 +0200

    added base project with uart support

---
+++ Projet_voilier/base_uart/MyDrivers/MyUart.h
@@ -0,0 +1,42 @@
+
+#ifndef UART_H
+#define UART_H
+
+#include "stm32f103xb.h" 
+
+
+#define PinUartTX LL_GPIO_PIN_2
+#define USART_Pin GPIOA
+
+#define BUFFER_SIZE 64
+#define BUFFER_MASK 63
+
+struct Buffer{
+	char buff[BUFFER_SIZE];
+	int head;
+	int tail;
+};
+
+int isEmpty(void);
+void bufferInit(void);
+void bufferPush(char c);
+char bufferPull(void);
+
+
+
+/**
+	* @brief  Regle l'uart
+  * @note   Fonction à lancer avant toute autre.
+	* @param  USART_TypeDef Uart : indique l'uart à utiliser (UART2 pour utiliser le STLINK/V2)
+  * @retval None
+  */
+void MyUart_Conf(USART_TypeDef * Uart,int baudrate);
+
+void MyUart_sendChar(char c);
+void MyUart_sendString(const char* str);
+void MyUart_sendNumber(int n,int digits);
+
+
+
+
+#endif
